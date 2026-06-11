046165b781f271084125eee14ff84f274c2c98ed
moxian <moxian2007@yeah.net>
2026-06-11 16:37:30 +0800

    update

--- src/MediaController.php
+++ src/MediaController.php
@@ -2,8 +2,8 @@
 
 namespace Encore\Admin\Media;
 
-use Encore\Admin\Facades\Admin;
-use Encore\Admin\Layout\Content;
+use Ladmin\Facades\Admin;
+use Ladmin\Layout\Content;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
